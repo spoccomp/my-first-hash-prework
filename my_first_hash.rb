a88aede2085f2fc07e49aca283a6e855469484c1
Richard Trapanese <richardtrapanese@gmail.com>
2018-05-14 00:31:03 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = {fName => "Richard", lName => "Trapanese"}
+  my_name = {fName => "Richard", lName => "Trapanese"}
 
 end
 
